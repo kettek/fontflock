b478f30659feec4d680ac2e939d3bf99286cdadd
kts of kettek (POWERQWACK) <kts@kettek.net>
2020-11-14 19:38:36 -0800

    Adjust parsing

--- src/models/_SystemFonts.js
+++ src/models/_SystemFonts.js
@@ -1,20 +1,38 @@
 //const EventEmitter = require('events')
 //const { emit } = require('process')
+import path from 'path'
+import chokidar from 'chokidar'
 import * as fontManager from '../../build/fontmanager.node'
 import EventEmitter from 'events'
+import { FontFamily, FontFamilyEntry } from 'models/Fonts'
 
 class SystemFonts extends EventEmitter {
   constructor() {
     super()
-    this._fonts = []
+    this._families = []
   }
-  get fonts() {
-    return this._fonts
+  get families() {
+    return this._families
   }
   load() {
     this.emit('loading')
     fontManager.getAvailableFonts(fonts => {
-      this._fonts = fonts
+      // TODO: What we want to do is actually use chokidar to watch all the font path dirnames.
+      let fontPaths = []
+      let fontFamilies = {}
+      for (const c of fonts) {
+        // Gather root paths.
+        var fontRootPath = path.dirname(c.path)
+        if (!fontPaths.includes(fontRootPath)) {
+          fontPaths.push(fontRootPath)
+        }
+        // Gather families.
+        if (!fontFamilies[c.family]) {
+          fontFamilies[c.family] = new FontFamily(c.family)
+        }
+        fontFamilies[c.family].addEntry(c)
+      }
+      this._families = fontFamilies
       this.emit('loaded')
     })
   }
