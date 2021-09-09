c8137b066db2471129150e66f298143dbdeccb37
kts of kettek (POWERQWACK) <kts@kettek.net>
2021-09-09 05:40:05 -0700

    Pull current language from app.getLocale

--- src/models/_Locale.js
+++ src/models/_Locale.js
@@ -2,6 +2,7 @@ import EventEmitter from 'events'
 import i18next from 'i18next'
 import Backend from 'i18next-fs-backend'
 import yaml from 'yaml'
+import electron from 'electron'
 
 class Locale extends EventEmitter {
   constructor() {
@@ -10,6 +11,8 @@ class Locale extends EventEmitter {
       .use(Backend)
       .init({
         fallbackLng: 'en',
+        // TODO: Replace locale with a result from main<->renderer IPC. Also have a custom way of specifying the language to use.
+        lng: electron.remote.app.getLocale(),
         debug: true,
         backend: {
           addPath: 'build/locales/{{lng}}/{{ns}}.missing.yaml',
